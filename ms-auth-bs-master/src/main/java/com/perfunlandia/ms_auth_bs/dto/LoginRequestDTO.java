4c68124135d7d5512000ab4ddb5ff56084b2d947
ignaciojgr <ignac.gonzalezr@duocuc.cl>
2025-07-10 12:51:10 -0400

    git fix

--- ms-auth-bs-master/src/main/java/com/perfunlandia/ms_auth_bs/dto/LoginRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.perfunlandia.ms_auth_bs.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDTO {
-    private String email;
-    private String password;
-}
--- ms-auth-bs-master/src/main/java/com/perfunlandia/ms_auth_bs/model/Role.java
+++
@@ -1,9 +0,0 @@
-package com.perfunlandia.ms_auth_bs.model;
-
-
-public enum Role {
-    ADMIN,
-    USER,
-    SUPPORT
-}
-
